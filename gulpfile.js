75ad26cc769ebb3a5c5f734697f2ef5b0d31642a
blacksaildivision <krystian.zwak@blacksaildivision.com>
2016-06-08 19:29:54 +0200

    ID-3 add task to convert es6 to es5

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
 
-gulp.task('default', function() {
-    //
+gulp.task('js', function () {
+    return gulp.src('src/js/vjslider.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist'));
 });
+
