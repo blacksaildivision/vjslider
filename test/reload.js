628a0dcef0049362c10c69384b907e7dcfdb8078
blacksaildivision <krystian.zwak@blacksaildivision.com>
2016-11-29 14:14:05 +0100

    Add test for bug

--- test/reload.js
+++ test/reload.js
@@ -1,4 +1,4 @@
-casper.test.begin("Sliding testing", 7, function (test) {
+casper.test.begin("Sliding testing", 13, function (test) {
     "use strict";
 
     // Test general markup
@@ -26,6 +26,34 @@ casper.test.begin("Sliding testing", 7, function (test) {
             return __utils__.findOne(".vjslider__slider").style.width === "600%"
         }, "it has correct width of whole slider after reload");
     });
+
+    // Single slide test for multiple slides
+    casper.thenOpen("http://localhost:8363/demo/multiple-slides.html?" + Date.now(), function () {
+        test.assertHttpStatus(200, "it should load demo page");
+        test.assertExists(".vjslider", "it should contain main wrapper for slider");
+        test.assertEval(function () {
+            return __utils__.findAll(".vjslider__clone").length === 6;
+        }, "it contains correct number of cloned elements before reload");
+        test.assertEval(function () {
+            return __utils__.findOne(".vjslider__slider").style.width === "550%"
+        }, "it has correct width of before slider");
+    });
+
+    // Test reload with alternative options for multiple slides
+    // Hidden slide will become visible so it must be present in slider as well
+    casper.then(function () {
+        this.click(".js-reload");
+        test.assertExists(".vjslider", "it should contain main wrapper for slider after reload");
+        test.assertEval(function () {
+            return __utils__.findAll(".vjslider__clone").length === 12;
+        }, "it contains correct number of cloned elements after reload");
+
+        test.assertEval(function () {
+            return __utils__.findOne(".vjslider__slider").style.width.toFixed(4) === "333.3333%"
+        }, "it has correct width of whole slider after reload");
+    });
+
+
     casper.run(function () {
         test.done();
     });
