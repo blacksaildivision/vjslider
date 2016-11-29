b8d41f5f89c841fa0b22827dca1f4642e0c63b33
blacksaildivision <krystian.zwak@blacksaildivision.com>
2016-11-29 10:21:55 +0100

    Add destroy function

--- demo/nav.js
+++ demo/nav.js
@@ -1,34 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var slider = new VJSlider(document.querySelector('.carousel')),
         loopStarted = false,
         loop;
 
-    document.querySelector('.js-prev').addEventListener('click', function(event) {
+    document.querySelector('.js-prev').addEventListener('click', function (event) {
         event.preventDefault();
         slider.prev();
     });
 
-    document.querySelector('.js-next').addEventListener('click', function(event) {
+    document.querySelector('.js-next').addEventListener('click', function (event) {
         event.preventDefault();
         slider.next();
     });
 
-    document.querySelector('.js-start-loop').addEventListener('click', function(event) {
+    document.querySelector('.js-start-loop').addEventListener('click', function (event) {
         event.preventDefault();
         if (!loopStarted) {
             slider.next();
-            loop = setInterval(function() {
+            loop = setInterval(function () {
                 slider.next();
             }, 1000);
             loopStarted = true;
         }
     });
 
-    document.querySelector('.js-stop-loop').addEventListener('click', function(event) {
+    document.querySelector('.js-stop-loop').addEventListener('click', function (event) {
         event.preventDefault();
         if (loopStarted) {
             clearInterval(loop);
             loopStarted = false;
         }
+    });
+
+    document.querySelector('.js-destroy').addEventListener('click', function (event) {
+        event.preventDefault();
+        slider.destroy();
     });
 });
