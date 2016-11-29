9f9af8839d7e864d5468b2dd9073b93df06709a7
blacksaildivision <krystian.zwak@blacksaildivision.com>
2016-11-29 14:38:49 +0100

    Bump version after removing gulpflie and fixing bug

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const casperJs = require('gulp-casperjs');
-
-gulp.task('casperjs', () => {
-    return gulp.src('test/*.js')
-        .pipe(casperJs());
-});
-
-gulp.task('default', ['casperjs']);
